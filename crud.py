28f2d08a671d3a27eff77700d45b3ceb21c5fe67
ani-koiava <105850589+aniKoiava@users.noreply.github.com>
2023-02-19 18:41:49 +0400

    CV generator - 1

--- crud.py
+++ crud.py
@@ -12,23 +12,26 @@ class CRUD:
         for i in array:
             query += f"{i},"
         query += ");"
+        query -= ','
         self.cur.execute()
         
     
-    def create(self, table_name, *args):
-       pass
-
+    def insert(self, table):
+        ins = self.cur.execute(f"insert into {table} values")
+        return ins.fetchall()
     
     def select(self, table_name,  where=[] ):
         res = self.cur.execute(f"select * from {table_name} where{where}")
         return res.fetchall()
     
     
-    def update(self):
-        pass
-    
-    def delete(self):
-        pass
+    def update(self, table_name, where):
+        update = self.cur.execute(f"update * {table_name} set where {where}")
+        return update.fetchall()
+        
+    def delete(self, table_name):
+        delete = self.cur.execute(f'delete from {table_name} where t1 = ?')
+        return delete.fetchall()
     
     
     
